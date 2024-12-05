64fc8932ea8add907556e09548479b8163480a71
sigurdgroneng <sigurd.groneng@nav.no>
2024-12-05 16:28:18 +0100

    Disable metrics, but add placeholder file

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/index.tsx"),
+    route("/metrics", "routes/metrics.tsx"),
     route("/internal/isAlive", "routes/isAlive.tsx"),
     route("/internal/isReady", "routes/isReady.tsx")] satisfies RouteConfig;
