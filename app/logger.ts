e336b54542c0dbb990276a2b0ce95e993969fee9
sigurdgroneng <sigurd.groneng@nav.no>
2024-12-12 15:01:37 +0100

    Config level in logger

--- app/logger.ts
+++ app/logger.ts
@@ -1,2 +1,11 @@
 import pino from 'pino'
-export const logger = pino()
+
+// https://github.com/navikt/frontend/blob/205b1be8944105663571eef0e5627052358ab05a/nextjs/pino-logging/utils/backendLogger.ts
+export const logger = pino({
+        timestamp: false,
+        formatters: {
+            level: (label) => {
+                return { level: label };
+            },
+        },
+    });
