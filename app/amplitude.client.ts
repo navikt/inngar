a837af415a0aa71c34cca64d132ff66122f9cce2
sigurdgroneng <sigurd.groneng@nav.no>
2025-02-21 10:07:19 +0100

    Only init amplitude when not running locally

--- app/amplitude.client.ts
+++ app/amplitude.client.ts
@@ -1,22 +1,26 @@
 import { init, track } from "@amplitude/analytics-browser"
 import { EnvType, getEnv } from "~/util/envUtil.client"
 
+const env = getEnv()
+
 const apiKey =
-    getEnv().type === EnvType.local
-        ? "faf28eb5445abfe75c7ac28ae7a8d050"
-        : getEnv().type === EnvType.prod
+    env.type === EnvType.local
+        ? ""
+        : env.type === EnvType.prod
           ? "691963e61d2b11465aa96acfcaa8959b"
           : "faf28eb5445abfe75c7ac28ae7a8d050"
 
 const AMPLITUDE_API_KEY = apiKey
 
-init(AMPLITUDE_API_KEY, undefined, {
-    autocapture: true,
-    serverUrl: "https://amplitude.nav.no/collect",
-    ingestionMetadata: {
-        sourceName: window.location.toString(),
-    },
-})
+if (env.type != EnvType.local) {
+    init(AMPLITUDE_API_KEY, undefined, {
+        autocapture: true,
+        serverUrl: "https://amplitude.nav.no/collect",
+        ingestionMetadata: {
+            sourceName: window.location.toString(),
+        },
+    })
+}
 
 const logEvent = (eventName: string, eventProperties: Record<string, any>) => {
     track(eventName, {
