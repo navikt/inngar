d375ff93ef7b5ea91e8aa7c09e231bfe6ecab508
sigurdgroneng <sigurd.groneng@nav.no>
2025-02-21 11:00:00 +0100

    Redirect to /:code if aktivBruker in context

--- app/routes/ingenBrukerValgtPage.tsx
+++ app/routes/ingenBrukerValgtPage.tsx
@@ -1,5 +1,23 @@
+import { ModiacontextholderApi } from "~/api/modiacontextholder"
 import type { Route } from "../../.react-router/types/app/routes/+types/ingenBrukerValgtPage"
 import { Alert, Heading } from "@navikt/ds-react"
+import { aktivBrukerUrl } from "~/config"
+import { resilientFetch } from "~/util/resilientFetch"
+import { redirect } from "react-router"
+
+export const loader = async ({ request }: Route.LoaderArgs) => {
+    const aktivBruker = await resilientFetch<{ aktivBruker: string | null }>(
+        new Request(aktivBrukerUrl, new Request(request)),
+    )
+    if (aktivBruker.ok && aktivBruker.data.aktivBruker) {
+        const fnrCode = await ModiacontextholderApi.generateForFnr(
+            aktivBruker.data.aktivBruker,
+        )
+        if (fnrCode) {
+            return redirect(`/${fnrCode}`)
+        }
+    }
+}
 
 export default function Index({}: Route.ComponentProps) {
     return (
