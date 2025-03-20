cb8b4f6fbc0aefe20637b9a6fa94c89a345c87e8
sigurdgroneng <sigurd.groneng@nav.no>
2025-03-20 13:58:23 +0100

    Midlertidig tekst om lovlig opphold

--- app/registreringPage/UgyldigFregStatusWarning.tsx
+++ app/registreringPage/UgyldigFregStatusWarning.tsx
@@ -1,4 +1,4 @@
-import { Alert, BodyShort, Heading, List, Link } from "@navikt/ds-react"
+import { Alert, BodyShort, Heading, Link, List } from "@navikt/ds-react"
 import type { KanIkkeStartePgaFolkeregisterStatus } from "~/api/veilarboppfolging"
 import { ListItem } from "@navikt/ds-react/List"
 
@@ -16,8 +16,8 @@ const ugyldigStatusTekster: Record<
         tekst: "",
     },
     IKKE_LOVLIG_OPPHOLD: {
-        tittel: "Denne personen har ikke lovlig opphold i Norge",
-        tekst: "Følg servicerutinen og gi avslag på arbeidsrettet oppfølging.",
+        tittel: "Denne personen har kanskje ikke lovlig opphold i Norge",
+        tekst: "Dersom personen er EU/EØS borger kan dette være feil. Hvis personen ikke er EU/EØS border, følg servicerutinen og gi avslag på arbeidsrettet oppfølging.",
         lenker: [
             {
                 tekst: "Mal for avslag til utsendelse gjennom fagsystemet Gosys",
@@ -46,18 +46,30 @@ export const UgyldigFregStatusWarning = ({
 }) => {
     const tekster = ugyldigStatusTekster[kanStarteOppfolging]
     return (
-        <Alert variant={"error"}>
-            <Heading size="small">{tekster.tittel}</Heading>
-            <BodyShort>{tekster.tekst}</BodyShort>
-            {tekster.lenker && (
-                <List>
-                    {tekster.lenker.map((punkt) => (
-                        <ListItem>
-                            <Link href={punkt.lenke}>{punkt.tekst}</Link>
-                        </ListItem>
-                    ))}
-                </List>
-            )}
-        </Alert>
+        <>
+            <Alert variant={"error"}>
+                <Heading size="small">{tekster.tittel}</Heading>
+                <BodyShort>{tekster.tekst}</BodyShort>
+                {tekster.lenker && (
+                    <List>
+                        {tekster.lenker.map((punkt) => (
+                            <ListItem>
+                                <Link href={punkt.lenke}>{punkt.tekst}</Link>
+                            </ListItem>
+                        ))}
+                    </List>
+                )}
+            </Alert>
+            {kanStarteOppfolging === "IKKE_LOVLIG_OPPHOLD" ? (
+                <Alert variant="info" inline>
+                    <p>
+                        Vi har oppdaget en feil som berører EU/EØS-brukere som
+                        ikke er bosatt i Norge. Vi arbeider med å rette feilen,
+                        slik at disse brukerne skal kunne få tilgang til
+                        arbeidsrettet oppfølging.
+                    </p>
+                </Alert>
+            ) : null}
+        </>
     )
 }
