f0621049b391c55b34cd98c901e336c521583ef8
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2025-04-22 12:28:43 +0200

    Bruk enum for sammenligning av environment

--- app/config.client.ts
+++ app/config.client.ts
@@ -1,15 +1,15 @@
-import { getEnv } from "~/util/envUtil"
+import { EnvType, getEnv } from "~/util/envUtil"
 
 export const getOversiktenLink = () => {
     const env = getEnv()
-    return env.type === "prod"
+    return env.type === EnvType.prod
         ? "https://veilarbportefoljeflate.intern.nav.no"
         : `https://veilarbportefoljeflate.${env.ingressType}.dev.nav.no`
 }
 
 export const getVeilarbpersonflateUrl = () => {
     const env = getEnv()
-    return env.type === "prod"
+    return env.type === EnvType.prod
         ? "https://veilarbpersonflate.intern.nav.no"
         : `https://veilarbpersonflate.${env.ingressType}.dev.nav.no`
 }
