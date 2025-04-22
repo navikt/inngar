123a712a1a782cb8d64eb89d668721095fa7baf5
johannetronstad <johanne.tronstad@nav.no>
2025-04-22 12:40:05 +0200

    Sett fallback til prod url

--- app/config.client.ts
+++ app/config.client.ts
@@ -2,14 +2,14 @@ import { EnvType, getEnv } from "~/util/envUtil"
 
 export const getOversiktenLink = () => {
     const env = getEnv()
-    return env.type === EnvType.prod
-        ? "https://veilarbportefoljeflate.intern.nav.no"
-        : `https://veilarbportefoljeflate.${env.ingressType}.dev.nav.no`
+    return env.type === EnvType.dev || env.type === EnvType.local
+        ? `https://veilarbportefoljeflate.${env.ingressType}.dev.nav.no`
+        : "https://veilarbportefoljeflate.intern.nav.no"
 }
 
 export const getVeilarbpersonflateUrl = () => {
     const env = getEnv()
-    return env.type === EnvType.prod
-        ? "https://veilarbpersonflate.intern.nav.no"
-        : `https://veilarbpersonflate.${env.ingressType}.dev.nav.no`
+    return env.type === EnvType.dev || env.type === EnvType.local
+        ? `https://veilarbpersonflate.${env.ingressType}.dev.nav.no`
+        : "https://veilarbpersonflate.intern.nav.no"
 }
