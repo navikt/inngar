db51d5e9c178828eead43dd04bd71286549006ac
johannetronstad <johanne.tronstad@nav.no>
2025-10-10 15:07:02 +0200

    Oppdater lenke til vurdering av lovlig opphold

--- app/registreringPage/ManuellGodkjenningIkkeBosattAlert.tsx
+++ app/registreringPage/ManuellGodkjenningIkkeBosattAlert.tsx
@@ -17,7 +17,7 @@ const ManuellGodkjenningIkkeBosattAlert = ({
                 <Link
                     inlineText
                     underline
-                    href="https://lovdata.no/lov/2006-06-16-20/§14a"
+                    href="navno.sharepoint.com/sites/fag-og-ytelser-regelverk-og-rutiner/SitePages/Vurdering-av-oppholdsrett,-lovlig-opphold-og-avslag-etter-Nav-loven-§-14-a.aspx"
                 >
                     vurdering av lovlig opphold og avslag etter Nav-loven § 14 a
                 </Link>
