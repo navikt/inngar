1dea595f5215771e80dbbf71d011bf8bc787abd0
sigurdgroneng <sigurd.groneng@nav.no>
2026-01-28 13:24:40 +0100

    Fix toggle

--- app/hooks/useFeatureToggle.ts
+++ app/hooks/useFeatureToggle.ts
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react"
 import { logger } from "../../server/logger.ts"
 
-export function useFeatureToggle(featureName: string): boolean {
+type FeatureToggles = "inngar.overstyr-kontor"
+type Features = Record<FeatureToggles, boolean>
+
+export function useFeatureToggle(featureName: FeatureToggles): boolean {
     const [enabled, setEnabled] = useState(false)
 
     useEffect(() => {
         logger.info("Henter toggle...")
         fetch(`/veilarbaktivitet/api/feature?feature=${featureName}`)
             .then((res) => res.json())
-            .then((data) => {
-                setEnabled(data === true || data?.enabled === true)
+            .then((data: Features) => {
+                setEnabled(data === true || data?.[featureName] === true)
             })
             .catch(() => {
                 logger.warn("Klarte ikke hente feature-toggles")
