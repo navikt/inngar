d9c7630bec5f339d88754593b1a6f974f0263c2f
sigurdgroneng <sigurd.groneng@nav.no>
2026-04-09 13:53:29 +0200

    Legg til nais-fil for dev og påkrevde isAlive / isReady endepunkter

--- inngar-ekstern/app/routes.ts
+++ inngar-ekstern/app/routes.ts
@@ -2,5 +2,7 @@ import { index, route, type RouteConfig } from "@react-router/dev/routes"
 
 export default [
     index("routes/home.tsx"),
+    route("/internal/isAlive", "routes/isAlive.tsx"),
+    route("/internal/isReady", "routes/isReady.tsx"),
     route("*", "routes/notFound.tsx"),
 ] satisfies RouteConfig;
