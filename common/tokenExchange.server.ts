13504c99167d436fcf336cf749bbf9c9e47ae3cc
Mads.Lee.Giil <mads.lee.giil@nav.no>
2026-04-10 12:00:25 +0200

    Endre sjekk for å avgjøre hvilket scope som skal brukes

--- common/tokenExchange.server.ts
+++ common/tokenExchange.server.ts
@@ -58,8 +58,12 @@ export const getOboToken = async (
         }
     }
 
-    const isTokenX = validation.payload.iss?.includes("tokenx") ?? false
+    logger.info("Token issuer", { iss: validation.payload.iss })
+
+    const isTokenX = validation.payload.iss?.includes("tokendings") ?? false
     const scope = isTokenX ? tokenXScopeFrom(app) : azureScopeFrom(app)
+
+    logger.info("Valgt scope", { scope, isTokenX })
 
     const oboToken = await requestOboToken(token, scope)
     if (!oboToken.ok) {
